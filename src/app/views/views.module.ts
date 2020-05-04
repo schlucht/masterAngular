f3cf739b9a1c5bfe38638c64b1013ec65c298c1c
Schmid Lothar <web@jagolo.ch>
2020-05-04 12:46:34 +0000

    new item List

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -5,10 +5,11 @@ import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticComponent } from './statistic/statistic.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
-  declarations: [ViewsHomeComponent, StatisticComponent],
+  declarations: [ViewsHomeComponent, StatisticComponent, ItemListComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule,
